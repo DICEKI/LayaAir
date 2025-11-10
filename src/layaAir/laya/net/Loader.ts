bb59e92f70e6af82cc0871e1f1fe2bd054cfd110
DiCE <2798007640@qq.com>
2025-11-10 17:34:09 +0800

    metaMap中存原路径

--- src/layaAir/laya/net/Loader.ts
+++ src/layaAir/laya/net/Loader.ts
@@ -1422,7 +1422,7 @@ export class Loader extends EventDispatcher {
             let file = files[m + k];
             switch (c.t) {
                 case 0: //图片
-                    metaMap[URL.formatURL(file)] = c;
+                    metaMap[file] = c;
                     break;
                 case 1: //自动图集
                     AtlasInfoManager.addAtlas(file, c.prefix, c.frames);
